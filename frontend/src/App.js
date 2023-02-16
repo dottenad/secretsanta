51a08f8721d0ae8f26a7b76ad30edb333cd52873
dottenad <ottenadd@gmail.com>
2023-02-16 10:58:09 -0800

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,59 @@
-import Nav from './components/Nav'
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Navigate
+} from "react-router-dom";
 import { useAuthContext } from './hooks/useAuthContext'
-import { Outlet } from 'react-router-dom'
+import Nav from './components/Nav'
+import Index from "./pages/Index";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Gifts from "./pages/Gifts";
+import Groups from "./pages/groups/Groups";
+import CreateGroup from './pages/groups/Create';
 
 export default function App() {
     const { user } = useAuthContext()
     return (
         <>
-            {user && (
+            <BrowserRouter>
                 <html className="h-full bg-gray-100">
                     <body className="h-full">
                         <div className="min-h-screen">
                             <Nav />
                             <main className="py-10">
-                                <Outlet />
+                                <Routes>
+                                    <Route
+                                        path="/"
+                                        element={user ? <Index /> : <Navigate to="/login" />}
+                                    />
+                                    <Route
+                                        path="/gifts"
+                                        element={<Gifts />}
+                                    />
+                                    <Route
+                                        path="/groups"
+                                        element={<Groups />}
+                                    />
+                                    <Route
+                                        path="/groups/create"
+                                        element={<CreateGroup />}
+                                    />
+                                    <Route
+                                        path="/login"
+                                        element={!user ? <Login /> : <Navigate to="/" />}
+                                    />
+                                    <Route
+                                        path="/signup"
+                                        element={!user ? <Signup /> : <Navigate to="/" />}
+                                    />
+                                </Routes>
                             </main>
                         </div>
                     </body>
                 </html >
-            )}
-            {!user && (
-                <p>No user found</p>
-            )}
+            </BrowserRouter>
         </>
     )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,59 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate
-} from "react-router-dom";
-import { useAuthContext } from './hooks/useAuthContext';
 import './App.css';
+import App from './App'
 import { AuthContextProvider } from './context/AuthContext';
-import App from "./App";
-import Index from "./pages/Index";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import Gifts from "./pages/Gifts";
-import Groups from "./pages/groups/Groups";
-import CreateGroup from './pages/groups/Create';
-
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-          index: true,
-          element: <Index />
-        },
-        {
-          path: "/gifts",
-          element: <Gifts />,
-        },
-        {
-          path: "/groups",
-          element: <Groups />,
-        },
-        {
-          path: "/groups/create",
-          element: <CreateGroup />,
-        }
-      ]
-    },
-    {
-      path: "/signup",
-      element: <Signup />
-    },
-    {
-      path: "/login",
-      element: <Login />
-    }
-  ]
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthContextProvider>
-    <RouterProvider router={router} />
+    <App />
   </AuthContextProvider>
 );
