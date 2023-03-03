486555c3b03c890e8e086f31f83b898b441800cc
dottenad <ottenadd@gmail.com>
2023-03-03 10:20:19 -0800

    commit

--- frontend/src/components/CreateGroup.js
+++ frontend/src/components/CreateGroup.js
@@ -1,8 +1,88 @@
+import { useState } from 'react';
+import Member from '../components/CreateMember'
+import { v4 as uuidv4 } from 'uuid';
+
+const initialList = [];
+
 export default function CreateGroupForm() {
+
+    const [list, setList] = useState(initialList);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+
+    function handleFirstNameChange(event) {
+        setFirstName(event.target.value)
+    }
+
+    function handleLastNameChange(event) {
+        setLastName(event.target.value)
+    }
+
+    function handleEmailChange(event) {
+        setEmail(event.target.value)
+    }
+
+    function handleAdd(e) {
+        e.preventDefault()
+        const newList = list.concat({ firstName, lastName, email, id: uuidv4() })
+
+        setList(newList)
+
+        setFirstName('')
+        setLastName('')
+        setEmail('')
+    }
+
+    function handleDelete(id) {
+        setList(oldValues => {
+            return oldValues.filter(item => item.id !== id)
+        })
+    }
+
+
+
     return (
         <>
-            <div className="mx-auto max-w-3xl px-4 sm:px-6 mt-8 grid grid-cols-1 gap-6 lg:max-w-7xl">
+            <div className="mx-auto max-w-3xl px-4 sm:px-6 mt-8 grid grid-cols-1 gap-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-3">
                 <div className="space-y-6 lg:col-span-2 lg:col-start-1">
+                    <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                                <div className="lg:col-span-1 lg:col-start-3">
+                                    <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                        <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                                            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                                                <table className="min-w-full divide-y divide-gray-300">
+                                                    <thead className="bg-gray-50">
+                                                        <tr>
+                                                            <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">
+                                                                Group Name
+                                                            </th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody className="divide-y divide-gray-200 bg-white">
+                                                        <tr>
+                                                            <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none">
+                                                                <form>
+                                                                    <div className="mt-1">
+                                                                        <input
+                                                                            type="text"
+                                                                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm mb-3"
+                                                                        />
+                                                                    </div>
+                                                                </form>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                             <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
@@ -10,19 +90,98 @@ export default function CreateGroupForm() {
                                     <thead className="bg-gray-50">
                                         <tr>
                                             <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                                                Gift Name
+                                                Name
                                             </th>
                                             <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                Price
+                                                Email
                                             </th>
                                             <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                                URL
-                                            </th>
-                                            <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                                                <span className="sr-only">Edit</span>
+                                                &nbsp;
                                             </th>
                                         </tr>
                                     </thead>
+                                    <tbody className="divide-y divide-gray-200 bg-white">
+                                        {list.map((item) => {
+
+                                            // return item !== null ? (
+                                            //     <tr key={item.id}>
+                                            //         <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                                            //             {item.firstName}
+                                            //         </td>
+                                            //         <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{item.email}</td>
+                                            //         <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                            //             <span onClick={() => handleDelete(item.id)} className="text-indigo-600 hover:text-indigo-900 cursor-pointer">
+                                            //                 Delete
+                                            //             </span>
+                                            //         </td>
+                                            //     </tr>
+                                            // ) : (
+                                            //     <p>world</p>
+                                            // );
+                                        })}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="lg:col-span-1 lg:col-start-3">
+                    <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                                <table className="min-w-full divide-y divide-gray-300">
+                                    <thead className="bg-gray-50">
+                                        <tr>
+                                            <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900">
+                                                Add New Member
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className="divide-y divide-gray-200 bg-white">
+                                        <tr>
+                                            <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none">
+                                                <form>
+                                                    <label className="block text-sm font-medium text-gray-700">
+                                                        First Name
+                                                    </label>
+                                                    <div className="mt-1">
+                                                        <input
+                                                            type="text"
+                                                            value={firstName}
+                                                            onChange={handleFirstNameChange}
+                                                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm mb-3"
+                                                        />
+                                                    </div>
+                                                    <label className="block text-sm font-medium text-gray-700">
+                                                        Last Name
+                                                    </label>
+                                                    <div className="mt-1">
+                                                        <input
+                                                            type="text"
+                                                            value={lastName}
+                                                            onChange={handleLastNameChange}
+                                                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm mb-3"
+                                                        />
+                                                    </div>
+                                                    <label className="block text-sm font-medium text-gray-700">
+                                                        Email
+                                                    </label>
+                                                    <div className="mt-1">
+                                                        <input
+                                                            type="text"
+                                                            value={email}
+                                                            onChange={handleEmailChange}
+                                                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm mb-3"
+                                                        />
+                                                    </div>
+                                                    <button
+                                                        onClick={handleAdd}
+                                                        className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 mt-1"
+                                                    >Add Member</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    </tbody>
                                 </table>
                             </div>
                         </div>
